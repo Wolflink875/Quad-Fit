4977aa6982149c86b6f027c8371f0db3db416e5a
Wolflink875 <157165585+Wolflink875@users.noreply.github.com>
2024-01-19 21:10:43 -0500

    Updated Program with data-based locating

--- plot.py
+++ plot.py
@@ -1,11 +1,13 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import math
-import time
 from math import*
 import scipy.optimize as opt
 import easygui
+
+dataset = pd.read_csv('data/Data.csv')
+dataset2 = pd.read_csv('data/Data2.csv')
 
 fixLabels = 50
 labelsOn = False
@@ -14,11 +16,6 @@ phase2 = True
 phase3 = True
 
 
-findI3 = 0
-findF3 = 0.00375
-
-findI4 = 0.00608
-findF4 = 0.00700
 
 I = 0
 F = 0
@@ -26,8 +23,7 @@ I2 = 0
 F2 = 0
 I3 = 0
 F3 = 0
-dataset = pd.read_csv('data/Data.csv')
-dataset2 = pd.read_csv('data/Data2.csv')
+
 size = dataset.size / 2 - 1
 size2 = dataset2.size / 2 - 1
 
@@ -60,31 +56,20 @@ plt.ioff()
 
 if fieldValues is not None:
     findI = float(fieldValues[0])
-else:
-    findI = 0.00328
-if fieldValues is not None:
     guess = float(fieldValues[4])
-else:
-    guess = 0.004
-if fieldValues is not None:
     findF = float(fieldValues[1])
-else:
-    findF = 0.00698
-
-if fieldValues is not None:
     findI2 = float(fieldValues[2])
-else:
-    findI2 = 0.00413
-if fieldValues is not None:
     guess2 = float(fieldValues[5])
-else:
-    guess2 = 0.005
-if fieldValues is not None:
     findF2 = float(fieldValues[3])
 else:
+    findI = 0.00328
+    guess = 0.004
+    findF = 0.00698
+    findI2 = 0.00413
+    guess2 = 0.005
     findF2 = 0.00672
 
-
+#----Determine Data Slice Size---------------------
 n = 0
 for _ in range(int(size)):
 
@@ -99,7 +84,6 @@ for _ in range(int(size)):
 
 newSize = F - I + 1
 int(newSize)
-#---------------------------------------------------
 n = 0
 for _ in range(int(size2)):
 
@@ -115,6 +99,7 @@ for _ in range(int(size2)):
 newSize2 = F2 - I2 + 1
 int(newSize2)
 #---------------------------------------------------
+#----Creating Array For Data Slice------------------
 new = np.empty((newSize,2))
 j = 0
 for a in range(newSize):
@@ -122,11 +107,18 @@ for a in range(newSize):
     new[j][1] = dataset['V'][I+j]
     j = j+1
 
+new2 = np.empty((newSize2,2))
+j = 0
+for a in range(newSize2):
+    new2[j][0] = dataset2['Time'][I2+j]
+    new2[j][1] = dataset2['V'][I2+j]
+    j = j+1
+#---------------------------------------
+#---Creates Poly Fit For Data Slice-----
 if (phase1 == True):
     polyline = np.linspace(findI, findF, 100)
     fit = np.poly1d(np.polyfit (new[:,0], new[:,1], 2))
     a = fit.c
-
 
     def f(x):
         return (a[0] * (x ** 2) + a[1] * (x) + a[2])
@@ -134,30 +126,19 @@ if (phase1 == True):
     result = opt.minimize(f, guess)
     print(result.x)
 
-
-
-new2 = np.empty((newSize2,2))
-j = 0
-for a in range(newSize2):
-    new2[j][0] = dataset2['Time'][I2+j]
-    new2[j][1] = dataset2['V'][I2+j]
-    j = j+1
-
-if (phase1 == True):
     polyline2 = np.linspace(findI2, findF2, 100)
     fit2 = np.poly1d(np.polyfit (new2[:,0], new2[:,1], 2))
     b = fit2.c
-
     def g(x):
         return (b[0]*(x ** 2) + b[1]*(x) + b[2])
 
     result2 = opt.minimize(g,x0=guess2)
 
-
     print(result2.x)
 
 
 fig,ax = plt.subplots()
+plt.ion()
 plt.scatter(dataset['Time'], dataset['V'])
 plt.scatter(dataset2['Time'], dataset2['V'])
 
@@ -187,11 +168,29 @@ for xy in zip(dataset2['Time'], dataset2['V']):
 
 plt.show()
 
+fieldNames2 = ["Initial 1", "Final 1", "Initial 2", "Final 2"]
+fieldValues2 = easygui.multenterbox("Please select ranges to locate the minimum value of your data:",
+                                   'Min Val', fieldNames2)
+
+plt.close()
+plt.ioff()
 
 
 if (phase2 == True) and (phase1 == True):
 
     diff = result.x - result2.x
+
+    if fieldValues2 is not None:
+        findI3 = float(fieldValues2[0]) + diff
+        findF3 = float(fieldValues2[1]) + diff
+        findI4 = float(fieldValues2[2]) + diff
+        findF4 = float(fieldValues2[3]) + diff
+    else:
+        findI3 = 0
+        findF3 = 0.00375
+        findI4 = 0.00608
+        findF4 = 0.00700
+
     datafit = np.empty((int(size2), 2))
     print(diff)
 
@@ -203,7 +202,6 @@ if (phase2 == True) and (phase1 == True):
         else:
             datafit[n][1] = dataset2['V'][n]
 
-
         n = n + 1
     n=0
     for _ in range(int(size)):
@@ -211,20 +209,8 @@ if (phase2 == True) and (phase1 == True):
             dataset['V'][n] = -dataset['V'][n]
         n = n + 1
 
-    fig, ax = plt.subplots()
-    plt.scatter(dataset['Time'], dataset['V'])
-    plt.scatter(datafit[:, 0], datafit[:, 1])
-    plt.axvline(x=(result2.x+diff), color='black', linestyle='dashed')
-    plt.axvline(x=(-0.00330), color='black', linestyle='dashed')
-
-
-    plt.show()
-
-if (phase3 == True) and (phase2 == True) and (phase1 == True):
-
     sel = np.empty((int(size2), 2))
     sel2 = np.empty((int(size2), 2))
-
 
     n=0
     minVal = 100
@@ -257,14 +243,28 @@ if (phase3 == True) and (phase2 == True) and (phase1 == True):
             sel2[n][1] = nan
 
         if (sel2[n][1] < minVal2) and (sel2[n][1] != nan):
-            minVal2 = sel2[n][1]
+            minVal2 = (sel2[n][1])
             F3 = n
 
         n = n + 1
 
+    n = 0
+    yVal = nan
+    yVal2 = nan
+
     minVal2 = sel2[F3][0]
 
     mid = (minVal2-minVal)/2 + minVal
+
+    minVal2 = round(minVal2,5)
+    minVal = round(minVal,5)
+
+    for _ in range(int(size)):
+        if dataset['Time'][n] == (minVal):
+            yVal = dataset['V'][n]
+        if dataset['Time'][n] == (minVal2):
+            yVal2 = dataset['V'][n]
+        n = n + 1
 
     fig, ax = plt.subplots()
     plt.scatter(dataset['Time'], dataset['V'])
@@ -272,18 +272,14 @@ if (phase3 == True) and (phase2 == True) and (phase1 == True):
     plt.scatter(sel[:, 0], sel[:, 1])
     plt.scatter(sel2[:, 0], sel2[:, 1])
 
-
-    #plt.axvline(x=(result2.x + diff), color='black', linestyle='dashed')
+    print(minVal2)
+    print(minVal)
     plt.axvline(x=(mid), color='black', linestyle='dashed')
-
     plt.axvline(x=(minVal), color='black', linestyle='dashed')
     plt.axvline(x=(minVal2), color='black', linestyle='dashed')
-    plt.axhline(y=(0.304), color='black', linestyle='dashed')
-    plt.axhline(y=(-0.304), color='black', linestyle='dashed')
+    plt.axhline(y=(yVal), color='black', linestyle='dashed')
+    plt.axhline(y=(yVal2), color='black', linestyle='dashed')
     plt.axhline(y=(0), color='black')
     plt.scatter(sel[:, 0], sel[:, 1])
-
-
-
 
     plt.show()
